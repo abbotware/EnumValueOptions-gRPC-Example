bfe444e1ecdcb2fdf1037c43dd626d87f292e1de
Anthony Abate <anthony.abate@gmail.com>
2022-10-03 07:12:52 -0400

    repro for: https://github.com/protocolbuffers/protobuf/issues/10696

--- EnumValueOptionsApp/Program.cs
+++ EnumValueOptionsApp/Program.cs
@@ -1,12 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-EnumDescriptor ProfilesEnumDescriptor = ProfilesEnumReflection.Descriptor.FindTypeByName<EnumDescriptor>(typeof(ProfilesEnum).Name);
-foreach (ProfilesEnum profile_enum in Enum.GetValues(typeof(ProfilesEnum)))
-{
-    EnumValueDescriptor enum_value_descriptor = ProfilesEnumDescriptor.FindValueByNumber((int) profile_enum);
-    var selector_value = enum_value_descriptor.GetOptions().GetExtension<string>(EnumValueOptionsExtensions.EnumName);
-    Console.WriteLine(selector_value);
+{ // Works
+    EnumDescriptor ProfilesEnumDescriptor = ProfilesEnumReflection.Descriptor.FindTypeByName<EnumDescriptor>(typeof(ProfilesEnum).Name);
+    foreach (ProfilesEnum profile_enum in Enum.GetValues(typeof(ProfilesEnum)))
+    {
+        EnumValueDescriptor enum_value_descriptor = ProfilesEnumDescriptor.FindValueByNumber((int)profile_enum);
+        var selector_value = enum_value_descriptor.GetOptions().GetExtension<string>(EnumValueOptionsExtensions.EnumName);
+        Console.WriteLine(selector_value);
+    }
 }
+
+{  // Doesnt Work
+    EnumDescriptor ProfilesEnumDescriptor = ProfilesEnumReflection.Descriptor.FindTypeByName<EnumDescriptor>("Nested.Type");
+    foreach (ProfilesEnum profile_enum in Enum.GetValues(typeof(ProfilesEnum)))
+    {
+        EnumValueDescriptor enum_value_descriptor = ProfilesEnumDescriptor.FindValueByNumber((int)profile_enum);
+        var selector_value = enum_value_descriptor.GetOptions().GetExtension<string>(EnumValueOptionsExtensions.EnumName);
+        Console.WriteLine(selector_value);
+    }
+}
+
 
 Console.ReadLine();
